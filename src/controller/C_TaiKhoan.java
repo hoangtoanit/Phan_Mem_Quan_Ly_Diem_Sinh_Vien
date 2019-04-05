caa89a663548500a777804ed30fe46905d479d49
hoangtoanit <hoangvantoan.edu@gmail.com>
2019-04-06 00:20:17 +0700

    Chuc Nang Dang Nhap

--- src/controller/C_TaiKhoan.java
+++ src/controller/C_TaiKhoan.java
@@ -5,10 +5,44 @@
  */
 package controller;
 
+import DB_Connect.ConnectionDB;
+import java.sql.*;
+
+import entity.TaiKhoan;
+
 /**
  *
  * @author HoangVanToan
  */
 public class C_TaiKhoan {
-    
+
+    public C_TaiKhoan() {
+
+    }
+
+    public boolean getLogin(String username, String password) {
+        boolean check = false;
+        try {
+            //Kết nối đến cơ sở dữ liệu
+            Connection conn = null;
+
+            conn = ConnectionDB.getConnectionDB();
+
+            //Thực hiện câu lệnh truy vấn
+            PreparedStatement ps = conn.prepareStatement("SELECT * FROM TaiKhoan where username = ? and password = ?");
+
+            ps.setString(1, username);
+            ps.setString(2, password);
+
+            ResultSet rs = ps.executeQuery();
+            if (rs != null && rs.next()) {
+                check = true;
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return check;
+
+    }
+
 }
